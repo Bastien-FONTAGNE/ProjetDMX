d9c70283ad584953e102f4f1420feb29a6d1360e
CarpentierJ <jeremy.charpe@gmail.com>
2025-05-05 11:02:57 +0200

    reglage problème

--- WEB/front/src/App.js
+++ WEB/front/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Login from './login/Login.js';
 import Home from './home/Home.js';
 
-
 function App() {
   return (
-    <div>
-      <Login />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Login />
+        <Home />
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
